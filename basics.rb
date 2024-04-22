aaa96e2ea61f469bce51c444ec9657bc02e652d5
Thomas Verderesi <tverderesi@gmail.com>
2024-04-22 16:14:09 -0300

    basics on printing on the screen

---
+++ basics.rb
@@ -0,0 +1,29 @@
+# Print something onto the screen using 'print'
+print "Print something onto the screen using 'print': "
+print "Hello, World!"
+puts ""
+
+# Print something onto the screen using 'puts'
+
+puts "Print something onto the screen using 'puts':"
+puts "Hello, World!"
+puts ""
+# Print vs. Puts
+# The difference between 'print' and 'puts' is that 'puts'
+# adds a newline character at the end of the string, while 'print' does not.
+# Example:
+puts "Print vs. Puts"
+puts "Line 1 using puts"
+print "Line 2 using print"
+puts "Line 3 using puts"
+puts ""
+# Output:
+# Line 1 using puts
+# Line 2 using printLine 3 using puts
+
+#Drawing a triangle shape using 'puts'
+puts "Drawing a triangle shape using 'puts'"
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
